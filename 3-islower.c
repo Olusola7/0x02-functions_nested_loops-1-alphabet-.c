9a7a2eddd65b54edef052ab1bbc7f7f192dc709d
olushola ajakaiye <110997123+Olusola7@users.noreply.github.com>
2022-09-18 04:13:39 -0700

    Create 3-islower.c

---
+++ 3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_islower _ short description, single line
+* @c: contain value to be compared
+*return: always 0.
+*/
+int_islower(int c)
+{
+  if (c >'a' && c< 'z')
+  {
+    return (1)
+    }
+  eles
+  {
+    return (0)
+    }}
